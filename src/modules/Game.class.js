e4d1a84c20772c8dfa4748b17cb16c1b5fe27516
Volodymyr Kolosov <kolosovvova0@gmail.com>
2025-09-19 17:07:43 +0300

    first verson 2048

--- src/modules/Game.class.js
+++ src/modules/Game.class.js
@@ -1,68 +1,325 @@
 'use strict';
 
-/**
- * This class represents the game.
- * Now it has a basic structure, that is needed for testing.
- * Feel free to add more props and methods if needed.
- */
 class Game {
-  /**
-   * Creates a new game instance.
-   *
-   * @param {number[][]} initialState
-   * The initial state of the board.
-   * @default
-   * [[0, 0, 0, 0],
-   *  [0, 0, 0, 0],
-   *  [0, 0, 0, 0],
-   *  [0, 0, 0, 0]]
-   *
-   * If passed, the board will be initialized with the provided
-   * initial state.
-   */
-  constructor(initialState) {
-    // eslint-disable-next-line no-console
-    console.log(initialState);
+  startBoard = [
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+  ];
+
+  constructor() {
+    this.board = this.startBoard.map((row) => [...row]);
+    this.score = 0;
+    this.status = 'idle';
+    this.firstMove = false;
   }
 
-  moveLeft() {}
-  moveRight() {}
-  moveUp() {}
-  moveDown() {}
+  moveLeft() {
+    const rowMain = [];
+    let chengCheck = false;
 
-  /**
-   * @returns {number}
-   */
-  getScore() {}
+    this.board.forEach((row) => {
+      const copyRow = [...row];
+      const readyRow = this.slideRowLeft(row);
 
-  /**
-   * @returns {number[][]}
-   */
-  getState() {}
+      this.score += readyRow.record;
+      rowMain.push(readyRow.row);
 
-  /**
-   * Returns the current game status.
-   *
-   * @returns {string} One of: 'idle', 'playing', 'win', 'lose'
-   *
-   * `idle` - the game has not started yet (the initial state);
-   * `playing` - the game is in progress;
-   * `win` - the game is won;
-   * `lose` - the game is lost
-   */
-  getStatus() {}
+      if (copyRow.toLocaleString() !== readyRow.row.toLocaleString()) {
+        chengCheck = true;
+      }
+    });
 
-  /**
-   * Starts the game.
-   */
-  start() {}
+    if (chengCheck) {
+      this.board = rowMain;
+      this.addRandomTitle();
+      this.firstMove = true;
+    }
+  }
+  moveRight() {
+    const rowMain = [];
+    let chengCheck = false;
 
-  /**
-   * Resets the game.
-   */
-  restart() {}
+    this.board.forEach((row) => {
+      const copyRow = [...row];
+      const readyRow = this.slideRowRight(row);
 
-  // Add your own methods here
+      this.score += readyRow.record;
+      rowMain.push(readyRow.row);
+
+      if (copyRow.toLocaleString() !== readyRow.row.toLocaleString()) {
+        chengCheck = true;
+      }
+    });
+
+    if (chengCheck) {
+      this.board = rowMain;
+      this.addRandomTitle();
+      this.firstMove = true;
+    }
+  }
+  moveUp() {
+    const transformColums = this.creatColumn(this.board);
+    const mainBoard = this.slideRowUp(transformColums);
+
+    if (mainBoard.cheng) {
+      this.board = mainBoard.board;
+      this.score += mainBoard.record;
+      this.addRandomTitle();
+      this.firstMove = true;
+    }
+  }
+  moveDown() {
+    const transformColums = this.creatColumn(this.board);
+    const mainBoard = this.slideRowDown(transformColums);
+
+    if (mainBoard.cheng) {
+      this.board = mainBoard.board;
+      this.score += mainBoard.record;
+      this.addRandomTitle();
+      this.firstMove = true;
+    }
+  }
+
+  getScore() {
+    return this.score;
+  }
+
+  getState() {
+    return this.board.map((row) => [...row]);
+  }
+
+  getStatus() {
+    this.winChek(this.board);
+    this.loseChek(this.board);
+
+    return this.status;
+  }
+
+  start() {
+    this.score = 0;
+    this.board = this.startBoard.map((row) => [...row]);
+    this.status = 'playing';
+    this.firstMove = true;
+
+    this.addRandomTitle();
+    this.addRandomTitle();
+  }
+
+  restart() {
+    this.score = 0;
+    this.board = this.startBoard.map((row) => [...row]);
+    this.status = 'playing';
+    this.firstMove = false;
+
+    this.addRandomTitle();
+    this.addRandomTitle();
+  }
+
+  // ----------------------------------------------------
+
+  addRandomTitle() {
+    const cleanСells = [];
+
+    this.board.forEach((array, i) => {
+      array.forEach((cells, j) => {
+        if (cells === 0) {
+          cleanСells.push({ row: i, cell: j });
+        }
+      });
+    });
+
+    if (cleanСells.length === 0) {
+      return;
+    }
+
+    const randomCel = cleanСells[Math.floor(Math.random() * cleanСells.length)];
+    const num = Math.random() < 0.1 ? 4 : 2;
+
+    this.board[randomCel.row][randomCel.cell] = num;
+  }
+
+  // функция проверки попеди
+
+  winChek(nowBoard) {
+    nowBoard.forEach((row) => {
+      if (row.some((num) => num === 2048)) {
+        this.status = 'win';
+      }
+    });
+  }
+
+  // функция проверки поражения
+
+  loseChek(curentBoard) {
+    let cleanCell = false;
+    let loseChek = true;
+
+    curentBoard.forEach((row) => {
+      if (row.some((num) => num === 0)) {
+        cleanCell = true;
+        loseChek = false;
+      }
+    });
+
+    if (!cleanCell) {
+      for (let i = 0; i < curentBoard.length; i++) {
+        for (let j = 0; j < curentBoard[i].length - 1; j++) {
+          if (curentBoard[i][j] === curentBoard[i][j + 1]) {
+            loseChek = false;
+          }
+        }
+      }
+    }
+
+    if (!cleanCell) {
+      const columsBoard = this.creatColumn(curentBoard);
+
+      for (let i = 0; i < columsBoard.length; i++) {
+        for (let j = 0; j < columsBoard[i].length - 1; j++) {
+          if (columsBoard[i][j] === columsBoard[i][j + 1]) {
+            loseChek = false;
+          }
+        }
+      }
+    }
+
+    if (loseChek) {
+      this.status = 'lose';
+    }
+  }
+
+  // функция для смещения чисел
+
+  move(row) {
+    const newRow = [];
+
+    row.forEach((num) => {
+      if (num !== 0) {
+        newRow.push(num);
+      }
+    });
+
+    while (newRow.length < 4) {
+      newRow.push(0);
+    }
+
+    return newRow;
+  }
+
+  // функция для соиденения пар
+
+  merge(row) {
+    let record = 0;
+
+    for (let i = 0; i < row.length; i++) {
+      if (row[i] !== 0 && row[i] === row[i + 1]) {
+        row[i] = row[i] * 2;
+        row[i + 1] = 0;
+        record += row[i];
+        i++;
+      }
+    }
+
+    return { row: row, record: record };
+  }
+
+  // фунция для создания колонок
+
+  creatColumn(board) {
+    const newColums = [[], [], [], []];
+
+    for (let i = 0; i < board.length; i++) {
+      for (let j = 0; j < board[i].length; j++) {
+        newColums[j].push(board[i][j]);
+      }
+    }
+
+    return newColums;
+  }
+
+  // функция для парсинга столбцов обратно в колонки
+
+  parsRowInColums(colums) {
+    const readyBoard = [[], [], [], []];
+
+    for (let i = 0; i < colums.length; i++) {
+      for (let j = 0; j < colums[i].length; j++) {
+        readyBoard[j].push(colums[i][j]);
+      }
+    }
+
+    return readyBoard;
+  }
+
+  // соиденям движения в лево
+
+  slideRowLeft(row) {
+    let mainRow = this.move(row);
+
+    mainRow = this.merge(mainRow);
+
+    const newRecord = mainRow.record;
+
+    mainRow = this.move(mainRow.row);
+
+    return { row: mainRow, record: newRecord };
+  }
+
+  // соиденяем движения в право
+  slideRowRight(row) {
+    const readyRow = this.slideRowLeft(row);
+
+    return { row: readyRow.row.reverse(), record: readyRow.record };
+  }
+  // движения в верх
+
+  slideRowUp(colums) {
+    let newColums = [];
+    let chengCheck = false;
+    let upRecord = 0;
+
+    colums.forEach((column) => {
+      const copyColumn = [...column];
+      const newColumn = this.slideRowLeft(column);
+
+      newColums.push(newColumn.row);
+      upRecord += newColumn.record;
+
+      if (copyColumn.toString() !== newColumn.row.toString()) {
+        chengCheck = true;
+      }
+    });
+
+    newColums = this.parsRowInColums(newColums);
+
+    return { board: newColums, record: upRecord, cheng: chengCheck };
+  }
+
+  // движения в низ
+
+  slideRowDown(colums) {
+    let newColums = [];
+    let chengCheck = false;
+    let upRecord = 0;
+
+    colums.forEach((colum) => {
+      const copyColumn = [...colum];
+      const newColumn = this.slideRowRight(colum);
+
+      newColums.push(newColumn.row);
+      upRecord += newColumn.record;
+
+      if (copyColumn.toString() !== newColumn.row.toString()) {
+        chengCheck = true;
+      }
+    });
+
+    newColums = this.parsRowInColums(newColums);
+
+    return { board: newColums, record: upRecord, cheng: chengCheck };
+  }
 }
 
 module.exports = Game;
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,133 @@
 'use strict';
 
-// Uncomment the next lines to use your game instance in the browser
-// const Game = require('../modules/Game.class');
-// const game = new Game();
+const Game = require('../modules/Game.class');
+const game = new Game();
 
-// Write your code here
+const body = document.querySelector('body');
+
+const container = body.querySelector('.container');
+// елементи голови нашой гри
+const headGame = container.querySelector('.game-header');
+const gameControls = headGame.querySelector('.controls');
+const relScr = gameControls.querySelector('.info').querySelector('.game-score');
+const btnStart = gameControls.querySelector('.button');
+// ігрова дошка нашой гри
+const allGamefield = container.querySelector('.game-field');
+const tBody = allGamefield.querySelector('tbody');
+const allRow = tBody.querySelectorAll('.field-row');
+// повідомлення про стан гри
+const allMessage = container.querySelector('.message-container');
+const winMasseg = allMessage.querySelector('.message-win');
+const loseMasseg = allMessage.querySelector('.message-lose');
+const startMasseg = allMessage.querySelector('.message-start');
+// функція для оновлення дошки
+
+function renderBoard(board) {
+  allRow.forEach((row, i) => {
+    const allCells = row.querySelectorAll('.field-cell');
+
+    allCells.forEach((cell, j) => {
+      cell.textContent = board[i][j] === 0 ? '' : board[i][j];
+      cell.className = 'field-cell';
+
+      const valeCell = Number(cell.textContent);
+
+      if (valeCell > 0) {
+        cell.classList.add(`field-cell--${valeCell}`);
+      }
+    });
+  });
+  udateRec();
+}
+// функція перевірки програшу/виграшу
+
+function statusCheker() {
+  cleanAllMes();
+
+  if (game.getStatus() === 'win') {
+    addMesage(winMasseg);
+  }
+
+  if (game.getStatus() === 'lose') {
+    addMesage(loseMasseg);
+  }
+}
+// обробка початку гри
+
+btnStart.addEventListener('click', (e) => {
+  cleanAllMes();
+
+  if (btnStart.classList.contains('start')) {
+    game.start();
+    btnStart.textContent = 'Restart';
+    btnStart.classList.replace('start', 'restart');
+  } else if (btnStart.classList.contains('restart')) {
+    game.restart();
+    btnStart.textContent = 'Start';
+    btnStart.classList.replace('restart', 'start');
+    addMesage(startMasseg);
+  }
+
+  renderBoard(game.getState());
+});
+
+// прибрати усі повідомлення
+
+function cleanAllMes() {
+  startMasseg.classList.add('hidden');
+  winMasseg.classList.add('hidden');
+  loseMasseg.classList.add('hidden');
+}
+
+// вивести повідомлення
+
+function addMesage(message) {
+  message.classList.remove('hidden');
+}
+
+// зміна рахунку
+
+function udateRec() {
+  relScr.textContent = game.getScore();
+}
+
+// обробка руху
+
+document.addEventListener('keydown', (e) => {
+  let moveTreker = false;
+
+  if (!game.firstMove) {
+    return;
+  }
+
+  if (game.getStatus() === 'win' || game.getStatus() === 'lose') {
+    return;
+  }
+
+  switch (e.key) {
+    case 'ArrowUp':
+      game.moveUp();
+      moveTreker = true;
+      break;
+
+    case 'ArrowDown':
+      game.moveDown();
+      moveTreker = true;
+      break;
+
+    case 'ArrowLeft':
+      game.moveLeft();
+      moveTreker = true;
+      break;
+
+    case 'ArrowRight':
+      game.moveRight();
+      moveTreker = true;
+      break;
+  }
+
+  if (moveTreker) {
+    renderBoard(game.getState());
+    statusCheker();
+  }
+});
